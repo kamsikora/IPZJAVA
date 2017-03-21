1f2306261c1bdb1ef417ec704525fd67148228dd
Marcin Jarzyński <marcinjarzyski@gmail.com>
2017-03-21 21:37:47 +0100

    0.1.5

--- src/ipz/Okno.java
+++ src/ipz/Okno.java
@@ -6,10 +6,14 @@
 package ipz;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 /**
@@ -33,9 +37,25 @@ public class Okno {
     
     @FXML
     private void wyjscie(ActionEvent event) throws Exception {
-        Stage stage = (Stage) exit.getScene().getWindow();
-        stage.close();
+    Alert alert = new Alert(AlertType.CONFIRMATION);
+    alert.setTitle("Zamykanie aplikacji");
+    alert.setHeaderText("Czy chcesz wyłączyć program?");
+
+    ButtonType buttonTypeOne = new ButtonType("Tak");
+    ButtonType buttonTypeCancel = new ButtonType("Nie");
+    
+    alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeCancel);
+    Optional<ButtonType> result = alert.showAndWait();
+    if (result.get() == buttonTypeOne){
+    Stage stage = (Stage) exit.getScene().getWindow();
+    stage.close();
+    } 
+    else 
+    {
+    alert.close();
     }
+    }
+    
     
     public void initialize(URL url, ResourceBundle rb) {
         // TODO      
